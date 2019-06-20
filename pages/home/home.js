4726e29eb61d1fbe02aa69808b0d3601c8d37448
Joy <1102482619@qq.com>
2019-06-20 10:36:13 +0800

    第一次提交

---
+++ pages/home/home.js
@@ -0,0 +1,17 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    bnrUrl: ["/images/a1.jpg", "/images/a2.jpg", "/images/a3.jpg", "/images/a4.jpg"],
+    background: ['demo-text-1', 'demo-text-2', 'demo-text-3', 'demo-text-4'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: false,
+    circular: false,
+    interval: 100,
+    duration: 500,
+    previousMargin: 0,
+    nextMargin: 0
+  },
+})
